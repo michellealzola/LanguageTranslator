4283086c427a7d6cc3c87fd0fdb5a6284f919b32
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-20 14:57:54 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Language_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblsTranslation = new System.Windows.Forms.Label();
+            this.btnItalian = new System.Windows.Forms.Button();
+            this.btnSpanish = new System.Windows.Forms.Button();
+            this.btnGerman = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(68, 69);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(228, 13);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Select a language and I will say Good morning.";
+            this.lblInstruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblsTranslation
+            // 
+            this.lblsTranslation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblsTranslation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsTranslation.Location = new System.Drawing.Point(47, 100);
+            this.lblsTranslation.Name = "lblsTranslation";
+            this.lblsTranslation.Size = new System.Drawing.Size(268, 23);
+            this.lblsTranslation.TabIndex = 1;
+            this.lblsTranslation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnItalian
+            // 
+            this.btnItalian.Location = new System.Drawing.Point(47, 146);
+            this.btnItalian.Name = "btnItalian";
+            this.btnItalian.Size = new System.Drawing.Size(75, 23);
+            this.btnItalian.TabIndex = 2;
+            this.btnItalian.Text = "Italian";
+            this.btnItalian.UseVisualStyleBackColor = true;
+            this.btnItalian.Click += new System.EventHandler(this.btnItalian_Click);
+            // 
+            // btnSpanish
+            // 
+            this.btnSpanish.Location = new System.Drawing.Point(141, 146);
+            this.btnSpanish.Name = "btnSpanish";
+            this.btnSpanish.Size = new System.Drawing.Size(75, 23);
+            this.btnSpanish.TabIndex = 2;
+            this.btnSpanish.Text = "Spanish";
+            this.btnSpanish.UseVisualStyleBackColor = true;
+            this.btnSpanish.Click += new System.EventHandler(this.btnSpanish_Click);
+            // 
+            // btnGerman
+            // 
+            this.btnGerman.Location = new System.Drawing.Point(240, 146);
+            this.btnGerman.Name = "btnGerman";
+            this.btnGerman.Size = new System.Drawing.Size(75, 23);
+            this.btnGerman.TabIndex = 2;
+            this.btnGerman.Text = "German";
+            this.btnGerman.UseVisualStyleBackColor = true;
+            this.btnGerman.Click += new System.EventHandler(this.btnGerman_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 219);
+            this.Controls.Add(this.btnGerman);
+            this.Controls.Add(this.btnSpanish);
+            this.Controls.Add(this.btnItalian);
+            this.Controls.Add(this.lblsTranslation);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "Form1";
+            this.Text = "Language Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblsTranslation;
+        private System.Windows.Forms.Button btnItalian;
+        private System.Windows.Forms.Button btnSpanish;
+        private System.Windows.Forms.Button btnGerman;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Language_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnItalian_Click(object sender, EventArgs e)
+        {
+            lblsTranslation.Text = "Buongiorno";
+        }
+
+        private void btnSpanish_Click(object sender, EventArgs e)
+        {
+            lblsTranslation.Text = "Buenos Dias";
+        }
+
+        private void btnGerman_Click(object sender, EventArgs e)
+        {
+            lblsTranslation.Text = "Guten Morgen";
+        }
+    }
+}
